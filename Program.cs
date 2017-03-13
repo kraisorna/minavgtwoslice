1ccb2fb271c167b8e110448c7ee02fbd07c7f574
Kraisorn Arjharn <kraisorna@aol.com>
2017-03-13 18:10:00 +0700

    First local commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinAvgTwoSlice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            //string S = "CAGCCTA";
+            //int[] P = { 2, 5, 0 };
+            //int[] Q = { 4, 5, 6 };
+            int[] A = { 4, 2, 2, 5, 1, 5, 8 };
+            Console.WriteLine(s.solution(A));
+            Console.ReadLine();
+        }
+    }
+
+    class Solution
+    {
+        public int solution(int[] A)
+        {
+            int result = 0;
+            int min = 10001;
+            int N = A.Length;
+            for (int i = 1; i < N; i++) {
+                if ((i < N - 1) && (A[i] + A[i + 1]) / 2 < min) {
+                    min = (A[i] + A[i + 1]) / 2;
+                    result = i;
+                }
+                if ((i < N - 2) && (A[i] + A[i + 1] + A[i + 2]) / 3 < min) {
+                    min=(A[i] + A[i + 1]+A[i+2]) / 3;
+                    result = i;
+                }
+            }
+            
+            return result;
+        }
+    }
+}
